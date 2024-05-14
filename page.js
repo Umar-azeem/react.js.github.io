fe1e1bc59771e090f04c04d9991b3c5dc3c48694
umar azeem <125313441+Umar-azeem@users.noreply.github.com>
2024-05-14 10:35:33 +0500

    Add files via upload

---
+++ page.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function About() {
+  return (
+    <div className='flex border-l-amber-800 justify-between bold'>
+      About
+    </div>
+  );
+}
+
+export default About;
